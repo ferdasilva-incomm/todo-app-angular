12b058ddc76ca486e4aa56ed7e08acf29193bbf3
Fernando da Silva <ferdasilva@incomm.com>
2025-12-04 10:21:25 -0300

    Angular Todo App

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,47 @@
+import { NgClass, NgFor } from '@angular/common';
 import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+
+export interface TodoItem {
+  id: number;
+  task: string;
+  completed: boolean;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, NgFor, NgClass],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('todo-app');
+  todoList : TodoItem[] = [];
+  newTask: string = '';
+
+  addTask(): void {
+    if(this.newTask.trim() !== '') {
+      const newTodoItem: TodoItem = {
+        id: Date.now(),
+        task: this.newTask,
+        completed: false
+      }
+      this.todoList.push(newTodoItem);
+      this.newTask = '';
+    }
+    console.log('Current Todo List:', this.todoList);
+  }
+
+  toggleCompleted(index: number): void {
+    this.todoList[index].completed = !this.todoList[index].completed;
+    console.log('Toggling completion for index:', this.todoList[index].completed);
+  }
+
+  deleteTask(id: number): void {
+    this.todoList = this.todoList.filter(item => item.id !== id);
+    console.log('Deleted task with id:', id);
+  }
+
+  
+
 }
